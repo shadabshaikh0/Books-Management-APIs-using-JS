3b65ad3f41652e27624cda775af9946f3e1ebe18
shadab.shaikh <shadab.shaikh@dealshare.in>
2022-03-14 11:40:21 +0530

    initial project config

--- config/environments/development.js
+++ config/environments/development.js
@@ -1,18 +1,3 @@
 module.exports = {
-  version: process.env.APP_VERSION,
-  port: process.env.PORT || 4000,
-  logging: {
-    maxsize: 100 * 1024, // 100mb
-    maxFiles: 2,
-    colorize: false
-  },
-  notifications: {
-    email: {
-      sendgrid: {
-        SG_ACCOUNT_ONE: process.env.SENDGRID_KEY_1,
-        SG_ACCOUNT_TWO: process.env.SENDGRID_KEY_2,
-        SG_ACCOUNT_THREE: process.env.SENDGRID_KEY_3
-      }
-    }
-  }
+  port: process.env.PORT || 4000
 }
